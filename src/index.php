609ef43d2376fe2197cd70ad1c2603b6c62057ba
Robert Groves <rwgdev@gmail.com>
2025-01-20 00:53:31 -0600

    Markup cleanup

--- src/index.php
+++ src/index.php
@@ -2,21 +2,19 @@
   $journal_file = $_SERVER['CONTEXT_DOCUMENT_ROOT'] . '/../../voidcast-journal.json';
   $journal_json_data = file_get_contents($journal_file);
 
-  if (!$journal_json_data) {
+  if ($journal_json_data === "") {
+    $journal_data = [];
+  } elseif (!$journal_json_data) {
     error_log("Error: Unable to read the journal file at {$journal_file}. Error: " . error_get_last()['message']);
     $journal_data = [];
+  } else {
+    $journal_data = array_map('json_decode', explode("\n", trim($journal_json_data)));
+
+    if (json_last_error() !== JSON_ERROR_NONE) {
+      error_log('Error: Failed to decode JSON data. Error: ' . json_last_error_msg());
+      $journal_data = [];
+    }
   }
-
-  $journal_data = array_map('json_decode', explode('\n', trim($journal_json_data)));
-
-  if (json_last_error() !== JSON_ERROR_NONE) {
-    error_log('Error: Failed to decode JSON data. Error: ' . json_last_error_msg());
-    $journal_data = [];
-  }
-
-  $filtered_journal_data = array_filter($journal_data, function ($entry) {
-    return isset($entry->title, $entry->date, $entry->content);
-  });
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,38 +26,28 @@
   </head>
   <body>
     <main>
-      <article>
+      <article class="vc-journal">
         <h1>Voidcast Journal</h1>
         <hr />
-        <?php
-        foreach ($journal_data as $entry) {
-          if (!isset($entry->title, $entry->date, $entry->content)) {
-            continue;
-          }
+<?php foreach ($journal_data as $entry) : ?>
+        <article class="vc-journal-entry">
+          <header>
+            <h1><?= htmlspecialchars($entry->title, ENT_QUOTES, 'UTF-8'); ?></h1>
+<?php
+            try {
+              $date_obj = new DateTimeImmutable($entry->date);
+              $date_obj = $date_obj->setTimezone(new DateTimeZone('US/Central'));
+              echo "            <time datetime=\"{$date_obj->format('c')}\">{$date_obj->format('M j, Y @ g:i A T')}</time>";
+            } catch (Exception $e) {
+              error_log("Error: Invalid date format for entry '{$entry->title}'. Error: " . $e->getMessage());
+            }
+?>
 
-          $title = htmlspecialchars($entry->title, ENT_QUOTES, 'UTF-8');
-          $date = htmlspecialchars($entry->date, ENT_QUOTES, 'UTF-8');
-          $content = nl2br(htmlspecialchars($entry->content, ENT_QUOTES, 'UTF-8'));
-
-          echo "<article>";
-          echo "  <header>";
-          echo "    <h1>" . $title . "</h1>";
-
-          try {
-            $date_obj = new DateTimeImmutable($date);
-            $date_obj = $date_obj->setTimezone(new DateTimeZone("US/Central"));
-            echo "    <time datetime=\"" . $date_obj->format("c") . "\">" . 
-            $date_obj->format("M j, Y @ g:i A T") . "</time>";
-          } catch (Exception $e) {
-            error_log("Error: Invalid date format for entry '{$title}'. Error: " . $e->getMessage());
-            echo "    <time>Invalid date</time>";
-          }
-          echo "  </header>";
-          echo "  <p>" . $content . "</p>";
-          echo "</article>";
-          echo "<hr />";
-        }
-        ?>
+          </header>
+          <p><?= nl2br(htmlspecialchars($entry->content, ENT_QUOTES, 'UTF-8')); ?></p>
+        </article>
+        <hr />
+<?php endforeach; ?>
       </article>
     </main>
   </body>
