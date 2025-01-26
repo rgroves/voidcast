9d90e693675dc4a73451258abf038d7560f5c370
Robert Groves <rwgdev@gmail.com>
2025-01-25 21:54:15 -0600

    Fix unclosed link tag

--- src/post/index.php
+++ src/post/index.php
@@ -57,7 +57,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="robots" content="noindex, nofollow" />
   <title>Cast Into The Void</title>
-  <link rel="stylesheet" href="./post.css">
+  <link rel="stylesheet" href="./post.css" />
 </head>
 <body>
   <main>
